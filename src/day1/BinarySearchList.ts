9017b095a225b3e128fcb82037266cb106fd8cf2
NizTheDev <arehman08@hotmail.com>
2022-11-23 20:47:09 +0500

    feat: solved binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+  let low = 0, high = haystack.length
+  do {
+    const mid = Math.floor(low + (high - low) / 2)
+    const value = haystack[mid]
+    if (value === needle) {
+      return true
+    } else if (value > needle) {
+      high = mid
+    } else if (value < needle) {
+      low = mid + 1
+    }
+  } while (low < high)
+  return false
 }
