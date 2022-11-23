97eb9907d627d74f341e3ea7456310ddf77e2177
NizTheDev <arehman08@hotmail.com>
2022-11-23 20:22:00 +0500

    feat: linear search | array basics

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,3 +1,10 @@
 export default function linear_search(haystack: number[], needle: number): boolean {
-
+  let i = 0
+  while (i < haystack.length) {
+    if (haystack[i] === needle) {
+      return true
+    }
+    ++i
+  }
+  return false
 }
