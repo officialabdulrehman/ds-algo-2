4ce832c819ccc3f0e06a740dec76dbedc2303cde
NizTheDev <arehman08@hotmail.com>
2022-11-25 23:56:38 +0500

    feat: Stack.ts

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,45 @@
+type Node<T> = {
+  value: T
+  prev?: Node<T>
+}
+
 export default class Stack<T> {
-    public length: number;
 
-    
+  public length: number;
+  private head?: Node<T>;
 
-    constructor() {
+  constructor() {
+    this.length = 0
+  }
+
+  push(item: T): void {
+    const node: Node<T> = {
+      value: item
+    }
+    this.length++
+    if (!this.head) {
+      this.head = node
+      return
     }
 
-    push(item: T): void {
+    node.prev = this.head
+    this.head = node
+  }
 
-}
-    pop(): T | undefined {
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1)
+    if (this.length === 0) {
+      const head = this.head
+      this.head = undefined
+      return head?.value
+    }
+    const head = this.head as Node<T>
+    this.head = head.prev
+    return head.value
+  }
 
-}
-    peek(): T | undefined {
+  peek(): T | undefined {
+    return this.head?.value
+  }
 
-}
 }
